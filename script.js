ad28db21d9997c22071abf06ec4a304dbc7eb041
D.V.N.S Narasimha Rao <devakinarasimharao7@gmail.com>
2026-06-27 10:33:12 +0530

    FAQ accordion in Frontend Mentor

---
+++ script.js
@@ -0,0 +1,18 @@
+const questionBoxes = document.querySelectorAll(".question");
+
+for (const question of questionBoxes) {
+  question.addEventListener('click', (event) => {
+    const parent = event.currentTarget.closest('.question');
+    if (parent.children[1]) {
+      parent.children[1].classList.toggle('show');
+    }
+    const img = parent.querySelector('img');
+    if (img) {
+      if (img.src.includes("icon-plus.svg")) {
+        img.src = "/faq-accordion-main/assets/images/icon-minus.svg";
+      } else {
+        img.src = "/faq-accordion-main/assets/images/icon-plus.svg";
+      }
+    }
+  });
+}
